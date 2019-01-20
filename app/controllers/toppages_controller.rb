ed858b3e694c80c9e1eb6323eb96829010702d0f
tech.naka <tech.naka9@gmail.com>
2019-01-20 13:19:55 +0000

    favorite

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -5,4 +5,17 @@ class ToppagesController < ApplicationController
       @microposts = current_user.feed_microposts.order('created_at DESC').page(params[:page])
     end
   end
+  
+  def like(micropost)
+    self.favorites.find_or_create_by(like_id: micropost.id)
+  end
+    
+  def unlike(micropost)
+    favorite = self.favorites.find_by(like_id: micropost.id)
+    favorite.destroy if favorite
+  end
+  
+  def like?(micropost)
+    self.likes.include?(micropost)
+  end
 end
